ab9f75a8f763b29a48a81448bedcd93c787fc3bf
Ethan Ferrari <eferrari@drwholdings.com>
2025-04-04 15:32:23 -0500

    fix eslint nits in queue.ts

--- src/queue.ts
+++ src/queue.ts
@@ -10,24 +10,24 @@ type AsyncQueueEntry<T> = {
 };
 
 export namespace AsyncQueue {
-  export type QueueImpl<T> = Pick<Array<T>, 'push'|'shift'|'length'>;
+  export type QueueImpl<T> = Pick<T[], 'push'|'shift'|'length'>;
 }
 
 
 export class AsyncQueue {
-  private static queueFactory: () => AsyncQueue.QueueImpl<any> = () => new LinkedList<any>();
+  private static queueFactory: () => AsyncQueue.QueueImpl<AsyncQueueEntry<any>> = () => new LinkedList<AsyncQueueEntry<any>>();
   public static setQueueImpl(queueFactory: () => AsyncQueue.QueueImpl<any>): void {
     AsyncQueue.queueFactory = queueFactory;
   }
 
   protected name: string;
-  private readonly queue: AsyncQueue.QueueImpl<any>;
+  private readonly queue: AsyncQueue.QueueImpl<AsyncQueueEntry<any>>;
   private running: Promise<void>;
 
-  constructor(name?: string) {
+  public constructor(name?: string) {
     this.name = name || 'AsyncQueue';
     this.queue = AsyncQueue.queueFactory();
-    this.enqueue = this.enqueue.bind(this);
+    this.enqueue = this.enqueue.bind(this) as AsyncQueue['enqueue'];
   }
 
   public async enqueue<T>(operation: Operation<T>): Promise<T> {
@@ -47,7 +47,7 @@ export class AsyncQueue {
   }
 
   private runQueue() {
-    if(!this.running) {
+    if(!Boolean(this.running)) {
       const queue = this.runQueueInternal();
       this.running = queue;
       this.running
@@ -59,19 +59,21 @@ export class AsyncQueue {
     }
   }
 
-  // Only one should be going at a time
+  // only one should be going at a time
   private async runQueueInternal() {
     while(this.queue.length) {
       const current = this.queue.shift();
       const {operation, resolve, reject} = current;
       try {
         const subtasks = new AsyncQueue();
+        /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+        /* eslint-disable @typescript-eslint/unbound-method */
         const result = await operation(subtasks.enqueue);
         // ensure any subtasks that weren't awaited in the operation invocation are flushed
         await subtasks.runQueue();
         resolve(result);
       } catch(e) {
-        reject(e);
+        reject(e as Error);
       }
     }
   }
